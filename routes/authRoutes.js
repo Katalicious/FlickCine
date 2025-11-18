e60a98c334927b07c36e10f567adb97cfa0f4a05
Estelle Lagoa <joanestellelagoa@gmail.com>
2025-11-18 17:40:26 +0000

    Form

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// Placeholder route handlers for /auth
-router.get('/', (req, res) => {
-	res.send('Auth root');
+router.get('/register', (req, res) => {
+    res.render('register', { title: 'Register' });
 });
 
 module.exports = router;
