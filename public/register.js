ff75af88397f1f5dec221354bd8c26c4f3804bf3
Estelle Lagoa <joanestellelagoa@gmail.com>
2025-11-18 20:11:17 +0000

    Commit form 8pm

--- public/register.js
+++ public/register.js
@@ -86,3 +86,25 @@ password.onkeyup = function() {
 };
 
 });
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.body.classList.add(currentTheme);
+    if (currentTheme === 'dark-mode') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
