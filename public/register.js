1e7865976228c15245a308f1344672e7d8d008a5
Estelle Lagoa <joanestellelagoa@gmail.com>
2025-11-18 18:27:14 +0000

    JS Form

--- public/register.js
+++ public/register.js
@@ -1,5 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+const nome = document.getElementById('name');
+const mensagemerronome = document.getElementById('BlurName');
+
+nome.addEventListener('blur', function() {
+    const trim = this.value.trim();
+    if(trim.length < 3 || trim.length > 20 || !/^[a-zA-Z0-9]+$/.test(trim)) {
+        mensagemerronome.innerHTML = "Este campo deve conter entre 3 a 20 caracteres alfanuméricos.";
+        mensagemerronome.style.color = 'red';
+    } else if(trim === '' || trim == null) {
+        mensagemerronome.innerHTML = "Este campo não pode estar vazio.";
+        mensagemerronome.style.color = 'red';
+    } else {
+        mensagemerronome.innerHTML = '';
+    }
+});
+
 let password = document.getElementById('password')
 let confirm_password = document.getElementById('confirm_password');
+let mensagemerropassword = document.getElementById('mensagem-de-erro-palavra-passe');
+
 
 function validatePassword(){
     if(password.value != confirm_password.value) {
@@ -11,3 +31,58 @@ function validatePassword(){
 
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
+
+/* Validação da palavra-passe com a mensagem de erro. */
+
+let letra = document.getElementById("letra");
+let maiúscula = document.getElementById("maiúscula");
+let numero = document.getElementById("numero");
+let comprimento = document.getElementById("comprimento");
+
+password.onfocus = function() {
+    document.getElementById("mensagem-de-erro-palavra-passe").style.display = "block";
+}
+
+password.onblur = function() {
+    document.getElementById("mensagem-de-erro-palavra-passe").style.display = "none";
+}
+
+password.onkeyup = function() {
+    let letrasminusculas = /[a-z]/g;
+    if(password.value.match(letrasminusculas)) {
+        letra.classList.remove("invalid");
+        letra.classList.add("valid");
+    } else {
+        letra.classList.remove("valid");
+        letra.classList.add("invalid");
+    }
+
+    let letrasmaiusculas = /[A-Z]/g;
+    if(password.value.match(letrasmaiusculas)) {
+        maiúscula.classList.remove("invalid");
+        maiúscula.classList.add("valid");
+    } else {
+        maiúscula.classList.remove("valid");
+        maiúscula.classList.add("invalid");
+    }
+
+    let numeros = /[0-9]/g;
+    if(password.value.match(numeros)) {
+        numero.classList.remove("invalid");
+        numero.classList.add("valid");
+    } else {
+        numero.classList.remove("valid");
+        numero.classList.add("invalid");
+    }
+
+    if(password.value.length >= 8) {
+        comprimento.classList.remove("invalid");
+        comprimento.classList.add("valid");
+    } else {
+        comprimento.classList.remove("valid");
+        comprimento.classList.add("invalid");
+    }
+
+};
+
+});
