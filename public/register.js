07660f2f297a30b82e0fda7c2e8c0fe319e7503a
Estelle Lagoa <joanestellelagoa@gmail.com>
2025-11-19 15:28:49 +0000

    15:28

--- public/register.js
+++ public/register.js
@@ -37,7 +37,6 @@ email.addEventListener('blur', function() {
 
 let password = document.getElementById('password')
 let confirm_password = document.getElementById('confirm-password');
-let mensagemerropassword = document.getElementById('mensagem-de-erro-palavra-passe');
 
 
 function validatePassword(){
